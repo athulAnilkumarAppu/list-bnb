dd5a4f7d866443420554af696d3f5091e3d62746
athul-anilkumar <athulanilkumarappu@gmail.com>
2025-04-24 08:53:54 +0530

    API details added

---
+++ src/config/config.js
@@ -0,0 +1,2 @@
+export const API_KEY = "2ce4ebd6-ebea-447e-92cb-c03fe2ae8bd3";
+export const API_BASE_URL = "https://ads.planetmedia.app/";
---
+++ src/config/urls.js
@@ -0,0 +1,8 @@
+export const REGISTER_USER = "/api/auth/local/register"; //post
+export const LOGIN_USER = "/api/auth/local"; //post
+export const CREATE_AD = "/api/advertisements"; //post
+export const GET_ADS = "/api/advertisements"; //get
+export const GET_AD_DETAIL = "/api/advertisements/:id"; //get
+export const GET_USER_PROFILE = "/api/profile"; //get
+export const UPDATE_USER_PROFILE = "/api/profile"; //put
+export const DELETE_AD = "/api/advertisements/:id"; //delete
