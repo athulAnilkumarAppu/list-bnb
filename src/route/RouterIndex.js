37eb680ac992e6496a1aa2aa01c0160240da5ad7
athul-anilkumar <athulanilkumarappu@gmail.com>
2025-04-24 21:16:51 +0530

    auth checker added

--- src/route/RouterIndex.js
+++ src/route/RouterIndex.js
@@ -4,15 +4,37 @@ import SignUp from "../components/signup/SignUp";
 import HomePage from "../components/dashboard/HomePage";
 import AdDetailPage from "../components/dashboard/AdDetailPage";
 import ProfileIndex from "../components/profile/ProfileIndex";
+import PrivateRoute from "../route/AuthChecker";
 
 export default function RouterIndex() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<SignUp />} />
-      <Route path="/homepage" element={<HomePage />} />
-      <Route path="/adDetailpage" element={<AdDetailPage />} />
-      <Route path="/profile" element={<ProfileIndex />} />
+      <Route
+        path="/homepage"
+        element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/adDetailpage"
+        element={
+          <PrivateRoute>
+            <AdDetailPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute>
+            <ProfileIndex />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
